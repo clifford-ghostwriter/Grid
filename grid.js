6e54e5c0e2541c71432479d9e53f9890e418d4cb
Clifford Izuchukwu <cliffizuchucks@gmail.com>
2022-11-20 08:52:42 +0100

    New

---
+++ grid.js
@@ -0,0 +1,15 @@
+function deep(){
+document.getElementById("search-deep").style.display="block";
+
+document.getElementById("search-simple").style.display="none";
+}
+
+function load(){
+document.getElementById("search-deep").style.display="none";
+}
+
+function simple(){
+document.getElementById("search-simple").style.display="block";
+
+document.getElementById("search-deep").style.display="none";
+}
